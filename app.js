a862666a8a3a76bad691f256f4c263d43929aa1c
Phil <philngtn@gmail.com>
2020-09-21 01:47:50 -0400

    salting and hash

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
-const mongoose = require("mongoose")
-const md5 = require("md5");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
+
 //Setup database
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -41,34 +43,36 @@ app.get("/register", function (req,res) {
 
 app.post("/register", function (req,res) { 
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-         
-            res.render("secrets")
-        }
-    })
-
+    bcrypt.hash(req.body.password, saltRound, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets")
+            }
+        })
+    })    
 })
 
 app.post("/login", function (req,res) { 
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email:username}, function (err, foundUser) {
         if(err){
             console.log(err)
         }else{
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result) { 
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });              
             }
         }    
     })
